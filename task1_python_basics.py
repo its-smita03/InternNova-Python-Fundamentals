9c50faf2f1b60c9b829f7ad421245a91805580dc
Smita Powar <smitapowar678@gmail.com>
2026-09-16 17:33:57 +0530

    Create task1_python_basics.py

---
+++ task1_python_basics.py
@@ -0,0 +1,10 @@
+print("Welcome to Python Data Analytics")
+
+name = input("Enter your Name: ")
+college = input("Enter your College Name: ")
+branch = input("Enter your Branch: ")
+
+print("\n--- Student Information ---")
+print("Name   :", name)
+print("College:", college)
+print("Branch :", branch)
