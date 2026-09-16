03c89ad05dc867a7f1c33feb4ff531bca94a8a70
Smita Powar <smitapowar678@gmail.com>
2026-09-16 17:37:00 +0530

    Create task4_conditional.py

---
+++ task4_conditional.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "Fail"
+
+print("Grade:", grade)
