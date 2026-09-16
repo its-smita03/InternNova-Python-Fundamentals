f6a19056e2032a9c820eccf1d26cd9b620b41c32
Smita Powar <smitapowar678@gmail.com>
2026-09-16 17:37:39 +0530

    Add loops for number printing and multiplication table

---
+++ task5_loops.py
@@ -0,0 +1,13 @@
+print("Numbers 1 to 20:")
+for i in range(1, 21):
+    print(i)
+
+n = int(input("\nEnter number for table: "))
+for i in range(1, 11):
+    print(n, "x", i, "=", n * i)
+
+print("\nEven numbers 1 to 50:")
+i = 2
+while i <= 50:
+    print(i)
+    i += 2
